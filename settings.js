588087a62dd277dc4e2db9ac922157ceee107b41
Teedybotz <99315585+Teedybotz@users.noreply.github.com>
2022-04-15 06:05:35 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -11,12 +11,12 @@ global.APIKeys = {
 }
 
 //===> UBAH DISINI
-global.namaowner = 'Ryuuka Team' //Ganti Jadi Namamu
-global.owner = ['6285609233482','6283891921787'] //Kalo Mau Lebih Tambahin Aja
-global.nomerowner = '6285609233482' //Owner Utama Istilahnya
-global.namabot = 'RyuBotz MD' //Ganti Jadi Nama Botmu
-global.packname = '© Ryubotz MD' //Sticker Weem
-global.author = 'Ryubotz By Ryuuka Team' //Sticker Weem
+global.namaowner = 'Tedxx' //Ganti Jadi Namamu
+global.owner = ['6281271007021','6281271007021'] //Kalo Mau Lebih Tambahin Aja
+global.nomerowner = '6281271007021' //Owner Utama Istilahnya
+global.namabot = 'TedzBotz MD' //Ganti Jadi Nama Botmu
+global.packname = '© Tedzbotz MD' //Sticker Weem
+global.author = 'Tedzbotz By Tedz' //Sticker Weem
 global.webme = 'https://api-ryubotz.herokuapp.com'
 global.sessionName = 'session'
 global.bg_welcome = 'https://telegra.ph/file/528b594d5d91c08482515.jpg' //Max 30kb Agar Tidak Delay
